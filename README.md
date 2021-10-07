d8f2a15bfb41dded6408fc509463a612066f03bc
saakethkoka <saaketh.koka@gmail.com>
2021-10-06 22:19:33 -0500

    Adding error handling when stream goes down.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TwitterAlertStockBot
+This bot is designed to buy a stock based on a reccomendation of a trusted user.
+
+
+
+## Installing
+```shell
+
+```
--- TweetListener.py
+++ TweetListener.py
@@ -2,7 +2,7 @@ from TweetHandler import handleTweet
 from twitter_cred import *
 from twilio_config import send_message
 from params import *
-
+import time
 
 class MaxListener(tweepy.StreamListener):
 
@@ -24,13 +24,17 @@ class MaxStream():
         self.stream = tweepy.Stream(auth=auth, listener=listener)
 
     def start(self):
-        id_number = api.get_user(user_to_track).id
-        self.stream.filter(follow=[str(id_number)])
+        while True:
+            try:
+                id_number = api.get_user(user_to_track).id
+                self.stream.filter(follow=[str(id_number)])
+            except:
+                time.sleep(300)
+
 
 if __name__ == '__main__':
 
     listener = MaxListener()
-
 
     stream = MaxStream(auth, listener)
     stream.start()
